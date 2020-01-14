35b93ad2444213bb315cee267005f418956e3efa
ljk <ljk@zzcwx.com>
2020-01-14 10:33:28 +0800

    readme

--- demo/head/index.js
+++ demo/head/index.js
@@ -10,12 +10,13 @@ Page({
     //当前page对象
     //要生产的图片宽高  单位rpx
     let head = new Head('mycanvas', this, size)
+    //设置头像
     head.setHead('../../images/head.jpg')
+    //添加挂件 需要是本地
     head.addWidget('../../images/head.png')
     head.addWidget('../../images/head.png')
     head.addWidget('../../images/head.png')
-    setTimeout(() => {
-      head.draw()
-    }, 100)
+    //渲染刷新 添加过挂件需要手动刷新
+    head.draw()
   }
 })
